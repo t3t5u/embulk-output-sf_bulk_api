e7cbafaf8ccaf4a689f482d38687e6a57b8b9e3c
Tetsuro Sano <t3t5u.5an0@gmail.com>
2023-01-13 10:51:52 +0900

    Improve error handling

--- src/main/java/org/embulk/output/sf_bulk_api/SForceTransactionalPageOutput.java
+++ src/main/java/org/embulk/output/sf_bulk_api/SForceTransactionalPageOutput.java
@@ -4,6 +4,7 @@ import com.sforce.soap.partner.fault.ApiFault;
 import com.sforce.soap.partner.sobject.SObject;
 import com.sforce.ws.ConnectionException;
 import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.collections.CollectionUtils;
 import org.embulk.config.TaskReport;
 import org.embulk.spi.Exec;
@@ -19,20 +20,27 @@ public class SForceTransactionalPageOutput implements TransactionalPageOutput {
   private final ForceClient forceClient;
   private final PageReader pageReader;
   private final PluginTask pluginTask;
+  private final ErrorHandler errorHandler;
 
   private final Logger logger = LoggerFactory.getLogger(SForceTransactionalPageOutput.class);
+  private boolean failed;
+  private long failures;
 
   public SForceTransactionalPageOutput(
-      ForceClient forceClient, PageReader pageReader, PluginTask pluginTask) {
+      ForceClient forceClient,
+      PageReader pageReader,
+      PluginTask pluginTask,
+      ErrorHandler errorHandler) {
     this.forceClient = forceClient;
     this.pageReader = pageReader;
     this.pluginTask = pluginTask;
+    this.errorHandler = errorHandler;
   }
 
   @Override
   public void add(Page page) {
     try {
-      ArrayList records = new ArrayList<>();
+      List<SObject> records = new ArrayList<>();
       pageReader.setPage(page);
       while (pageReader.nextRecord()) {
         final SObject record = new SObject();
@@ -41,10 +49,12 @@ public class SForceTransactionalPageOutput implements TransactionalPageOutput {
         records.add(record);
         if (records.size() >= BATCH_SIZE) {
           try {
-            forceClient.action(records);
+            failures += forceClient.action(records);
+            failed = failures != 0;
           } catch (ApiFault e) {
             // even if some records failed to register, processing continues.
-            logger.error(e.getExceptionCode().toString() + ":" + e.getExceptionMessage(), e);
+            failures += errorHandler.handleFault(records, e);
+            failed = true;
           }
           records = new ArrayList<>();
         }
@@ -52,12 +62,18 @@ public class SForceTransactionalPageOutput implements TransactionalPageOutput {
 
       if (CollectionUtils.isNotEmpty(records)) {
         try {
-          forceClient.action(records);
+          failures += forceClient.action(records);
+          failed = failures != 0;
         } catch (ApiFault e) {
-          logger.error(e.getExceptionCode().toString() + ":" + e.getExceptionMessage(), e);
+          failures += errorHandler.handleFault(records, e);
+          failed = true;
         }
       }
+    } catch (AbortException e) {
+      logger.error(e.getMessage(), e);
+      throw e;
     } catch (Exception e) {
+      failed = true;
       logger.error(e.getMessage(), e);
     }
   }
@@ -70,6 +86,7 @@ public class SForceTransactionalPageOutput implements TransactionalPageOutput {
     try {
       forceClient.logout();
     } catch (ConnectionException e) {
+      logger.warn(e.getMessage(), e);
     }
   }
 
@@ -78,6 +95,9 @@ public class SForceTransactionalPageOutput implements TransactionalPageOutput {
 
   @Override
   public TaskReport commit() {
-    return Exec.newTaskReport();
+    final TaskReport taskReport = Exec.newTaskReport();
+    taskReport.set("failed", failed);
+    taskReport.set("failures", failures);
+    return taskReport;
   }
 }
